93c2a71c762a40cac9ac38e1347cbaea554a66ce
gdgest3164 <rlaehdgs12@naver.com>
2023-11-19 00:12:30 +0900

    최종 설정 끗

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,10 @@
-// import { cssBundleHref } from "@remix-run/css-bundle";
-import stylesheet from "~/tailwind.css";
+import "@mantine/core/styles.css";
+import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 
-export const links: LinksFunction = () => [...(stylesheet ? [{ rel: "stylesheet", href: stylesheet }] : [])];
-// export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesheet }];
+export const links: LinksFunction = () => [...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : [])];
 
 export default function App() {
   return (
@@ -14,12 +14,15 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <ColorSchemeScript />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <MantineProvider>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </MantineProvider>
       </body>
     </html>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { IconAlarm } from "@tabler/icons-react";
+import { ActionIcon, Box, Button, Divider, Title } from "@mantine/core";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
@@ -6,26 +9,17 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1>Welcome to Gddong</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Box style={{ padding: "45px" }}>
+      <Box style={{ display: "flex", justifyContent: "space-between" }}>
+        <Title>나만의 블로그</Title>
+        <Link to="/posts/create">
+          <Button variant="light" color="red">
+            글쓰기
+          </Button>
+        </Link>
+      </Box>
+      <Divider mt={20} mb={15} />
+      PostList
+    </Box>
   );
 }
